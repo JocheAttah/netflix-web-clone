d2ddd5d565eb33258ada1d568de31e94c4305f51
Attah David <jocheattahdavid@gmail.com>
2021-08-24 14:13:53 +0100

    wip footer

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -2,7 +2,7 @@ import styled from "styled-components/macro";
 
 export const Item = styled.div`
   display: flex;
-  border-bottom: 8px solid #222;
+  border-bottom: 3px solid #222;
   padding: 50px 5%;color: white;
   overflow: hidden;
 `;
@@ -59,3 +59,4 @@ export const Container = styled.div`
     }
   }
 `;
+ 
