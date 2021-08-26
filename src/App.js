c7a29d2847c26e51f2496c19d92bbc9b8088315b
Attah David <jocheattahdavid@gmail.com>
2021-08-26 14:28:05 +0100

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { FaqsContainer } from "./containers/accordion";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/home";
+import * as ROUTES from "./constants/routes";
+import { Browse, Signin, Signup } from "./pages";
 
 export default function App() {
   return (
     <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
+      <Router>
+        <Route exact path={ROUTES.BROWSE}>
+          <Browse />
+        </Route>
+        <Route exact path={ROUTES.SIGN_IN}>
+          <Signin />
+        </Route>
+        <Route exact path={ROUTES.SIGN_UP}>
+          <Signup />
+        </Route>
+        <Route exact path={ROUTES.HOME}>
+          <Home />
+        </Route>
+      </Router>
     </>
   );
 }
